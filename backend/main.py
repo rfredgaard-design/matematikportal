48fa1cc2c1114e0ed64a02239e34390dd3847c88
rfredgaard-design <r.fredgaard@gmail.com>
2025-08-23 11:58:29 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,13 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from database import Base, engine
-from routers.importer import router as importer_router
-from routers.layout import router as layout_router
-from routers.questions import router as questions_router
+from .database import Base, engine
+from .routers.importer import router as importer_router
+from .routers.layout import router as layout_router
+from .routers.questions import router as questions_router
 
 Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN", "http://127.0.0.1:8080")
